525607627194006c683b3e77f20d00acdf8af8bf
ANAND <txsorcerer@gmail.com>
2026-02-18 15:22:53 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+// WAP to add two numbers
+#include <stdio.h>
+int main(){
+int x,y,add = 0;
+printf("Enter the value of x and y");
+scanf("%d %d",&x,&y);
+add = x + y;
+printf("the sum of x and y is: %d",add);
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,184 @@
+//1.wap for sum of two numbers
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int a, b, sum;
+//     cin >> a;
+//     cin >> b;
+//     sum = a + b;
+//     cout << sum;
+//     return 0;
+// }
+
+//2.wap for finding greater of two numbers
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int a, b, c;
+//     cin >> a;
+//     cin >> b;
+//     cin >> c;
+
+//     if (a >= b && a >= c)
+//         cout << a;
+//     else if (b >= a && b >= c)
+//         cout << b;
+//     else
+//         cout << c;
+
+//     return 0;
+// }
+
+// 3.wap for swapping values of  two numbers using third one
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int x, y, temp;
+
+//     cin >> x;
+//     cin >> y;
+//     temp = x;
+//     x = y;
+//     y = temp;
+//     cout << x << " " << y;
+//     return 0;
+// }
+
+// 4.wap for finding area of a rectangle
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int length, breadth, area;
+//     cin >> length;
+//     cin >> breadth;
+//     area = length * breadth;
+//     cout << area;
+//     return 0;
+// }
+
+// 5.wap to print square of a number
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n, square;
+//     cin >> n;
+//     square = n * n;
+//     cout << square;
+//     return 0;
+// }
+
+// 6.wap for finding area of a circle
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     float radius, area;
+//     float pi = 3.14;
+//     cin >> radius;
+//     area = pi * radius * radius;
+//     cout << area;
+//     return 0;
+// }
+
+// 7.wap to check for zero positive or negative
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cin >> n;
+//     if (n > 0)
+//         cout << "Positive";
+//     else if (n < 0)
+//         cout << "Negative";
+//     else
+//         cout << "Zero";
+//     return 0;
+// }
+
+// 8.wap to check for even or odd
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cin >> n;
+//     if (n % 2 == 0)
+//         cout << "Even";
+//     else
+//         cout << "Odd";
+//     return 0;
+// }
+
+// 9.wap to check wheather a given year is leap year or not.
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int year;
+//     cin >> year;
+//     if (year % 4 == 0)
+//         cout << "Leap Year";
+//     else
+//         cout << "Not Leap Year";
+//     return 0;
+// }
+
+// 10.wap to find ASCII value of given character
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     char ch;
+//     cin >> ch;
+//     cout << int(ch);
+//     return 0;
+// }
+
+// 11.wap to display simple class and object
+
+// #include <iostream>
+// using namespace std;
+// class Student {
+// public:
+//     int roll;
+//     int marks;
+// };
+// int main() {
+//     Student s;
+
+//     s.roll = 1;
+//     s.marks = 90;
+//     cout << s.roll << endl;
+//     cout << s.marks;
+//     return 0;
+// }
+
+//12.wap to add two numbers using class
+
+// #include <iostream>
+// using namespace std;
+// class Add {
+// public:
+//     int a, b;
+
+//     void sum() {
+//         cout << a + b;
+//     }
+// };
+// int main() {
+//     Add obj;
+
+//     obj.a = 3;
+//     obj.b = 4;
+//     obj.sum();
+//     return 0;
+// }
+
+
