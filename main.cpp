642a120dbcd34376bee2667aad8e9ebd72ee96f8
ANAND <txsorcerer@gmail.com>
2026-03-05 16:23:49 +0530

    bubble sort

--- main.cpp
+++ main.cpp
@@ -1163,15 +1163,18 @@
 // class SortArray {
 // public:
 
-//     void sort(int arr[], int size) {
-//         for (int i = 0; i < size - 1; i++) {
-//             for (int j = 0; j < size - i - 1; j++) {
-//                 if (arr[j] > arr[j + 1]) {
-//                     swap(arr[j], arr[j + 1]);
-//                 }
+
+// void sort(int arr[], int size) {
+//     for (int i = 0; i < size - 1; i++) {
+//         for (int j = 0; j < size - i - 1; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 int temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
 //             }
 //         }
 //     }
+// }
 
 //     void display(int arr[], int size) {
 //         for (int i = 0; i < size; i++) {
---
+++ practice.cpp
@@ -0,0 +1,132 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+// int a,b,add;
+// cout<<"Enter the value of a";
+// cin>>a;
+// cout<<"Enter the value of b";
+// cin>>b;
+// add = a+b;
+// cout<<"the sum of two numbers is:"<<add ;
+// return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// void SUM(float a, float b){
+//     cout<<a+b;
+// }
+
+
+
+// int main(){
+//     int a = 10,b = 20;
+//    SUM(a,b);
+// //    cout<<"the sum of two numbers is:"<<SUM(a,b);
+//     return 0;
+// }   
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+// int n;
+// int fact =1;
+// cout<<"enter your number";
+// cin>>n;
+// for(int i = 1;i <= n;i++){
+//     fact = fact*i;
+// }
+// cout<<"factorial of given number is:"<<fact;
+// return 0;
+// }
+// int factorial(int n){
+//     int fact = 1;
+//    for(int i = 1;i <= n;i++){
+//     fact = fact*i;
+// }     
+// return fact;
+// }
+
+// int main(){
+
+// cout<<factorial(5);
+// // cout<<check();
+
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// class Student {
+// public:    
+//     int rollno;
+//     char name[10];
+//     float cgpa;
+//     void input(){
+//             cout<<"enter roll no. of the student ";
+//             cin>>rollno;
+//             cout<<"enter nameof the student ";
+//             cin>> name;
+//             cout<<"enter cgpa the student ";
+//             cin>> cgpa;}
+//     void display(){
+//             cout<<"roll no. of student"<<rollno<<endl;
+//             cout<<"name of student"<<name<<endl;
+//             cout<<"cgpa of student"<<cgpa<<endl;
+
+//     }
+// };
+// int main(){
+// //    int num;
+// Student s1;
+// s1.input();
+// s1.display();
+// Student s2;
+// s2.input();
+// s2.display();
+
+// class Add{
+// public:
+//     float a,b;
+//     void input(){
+//         cout<<"enter the value of a "<<endl;
+//         cin>>a;
+//         cout<<"enter the value of b "<<endl;
+//         cin>>b;
+//     }
+    
+//     void sum(){
+//       cout<<"The sum of  a and b is:"<<a+b;
+//     }
+// };
+// int main(){
+// // CLASSNAME OBJECTNAME;
+// Add sum1;
+// sum1.input();
+// sum1.sum();
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
